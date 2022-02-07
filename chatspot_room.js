63fba1069e79e1d1170bd3de5865887295c2ec92
Shravani-Pawar <89009083+Shravani-Pawar@users.noreply.github.com>
2022-02-07 21:37:37 +0530

    Add files via upload

---
+++ chatspot_room.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyD50e641vMTSiEjeuccXxKEEVvhvkcixYA",
+    authDomain: "kwitter-6e461.firebaseapp.com",
+    databaseURL: "https://kwitter-6e461-default-rtdb.firebaseio.com",
+    projectId: "kwitter-6e461",
+    storageBucket: "kwitter-6e461.appspot.com",
+    messagingSenderId: "701841053044",
+    appId: "1:701841053044:web:06e51d5002fc2dd7f5c821"
+  };
+  
+  // Initialize Firebase
+   firebase.initializeApp(firebaseConfig);
